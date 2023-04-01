7d521525f2ea0d88d86d7bf48af2fe0a9675d9ae
mattiolka <mattiolka@gmail.com>
2023-04-01 21:51:49 +0300

    Додала обробники подій

---
+++ app.js
@@ -0,0 +1,12 @@
+const daysNumber = document.querySelector('#daysNumber').addEventListener('input', (e) => {
+    console.log(e.target.value);
+})
+
+const dayOfWeek = document.querySelector('#dayOfWeek').addEventListener('input', (e) => {
+    console.log(e.target.value);
+})
+
+const button = document.getElementById('button').addEventListener('click', (e) => {
+    console.log(e);
+})
+
